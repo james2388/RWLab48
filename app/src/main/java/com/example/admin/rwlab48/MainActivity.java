e79750ad6fd7dabd2e6ecaee8ba19ff032162725
Vladimir Farafonov <vfarafonov@ruswizards.com>
2014-12-10 11:48:00 +0400

    Added buttons with no action to main activity

--- app/src/main/java/com/example/admin/rwlab48/MainActivity.java
+++ app/src/main/java/com/example/admin/rwlab48/MainActivity.java
@@ -4,36 +4,55 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mBtnGallery = (Button)findViewById(R.id.btnGallery);
+        mBtnGallery.setOnClickListener(this);
+
+        Button mBtnWebBrowser = (Button)findViewById(R.id.btnWebBrowser);
+        mBtnWebBrowser.setOnClickListener(this);
+
+        Button mBtnMap = (Button)findViewById(R.id.btnMap);
+        mBtnMap.setOnClickListener(this);
+
+        Button mBtnGalleryImage = (Button)findViewById(R.id.btnGalleryImage);
+        mBtnGalleryImage.setOnClickListener(this);
+
+        Button mBtnPhoto = (Button)findViewById(R.id.btnCameraPhoto);
+        mBtnPhoto.setOnClickListener(this);
+
+        Button mBtnVideo = (Button)findViewById(R.id.btnCameraVideo);
+        mBtnVideo.setOnClickListener(this);
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnGallery:
+                break;
+            case R.id.btnWebBrowser:
+                break;
+            case R.id.btnMap:
+                break;
+            case R.id.btnGalleryImage:
+                break;
+            case R.id.btnCameraPhoto:
+                break;
+            case R.id.btnCameraVideo:
+                break;
+            case R.id.btnDictophone:
+                break;
+            default:
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
